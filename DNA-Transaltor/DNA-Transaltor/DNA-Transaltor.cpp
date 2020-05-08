f0a35238cb6e1e46fe11640d73c46d5ec9edcbe8
Youssef <youssef.ossama2@gmail.com>
2020-05-08 18:06:32 +0200

    trasnformed to maps

--- DNA-Transaltor/DNA-Transaltor/DNA-Transaltor.cpp
+++ DNA-Transaltor/DNA-Transaltor/DNA-Transaltor.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string.h>
+#include <iterator> 
+#include <map>
+using namespace std;
+
+map<string, string> codones;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    codones.insert(pair<string, string>("TTT","Phe"));
+    codones.insert(pair<string, string>("TTC", "Phe"));
+    codones.insert(pair<string, string>("TTA", "Leu"));
+    codones.insert(pair<string, string>("TTG", "Leu"));
+    codones.insert(pair<string, string>("TCT", "Ser"));
+    codones.insert(pair<string, string>("TCC", "Ser"));
+    codones.insert(pair<string, string>("TCA", "Ser"));
+    codones.insert(pair<string, string>("TCG", "Ser"));
+    string y;
+    cin >> y;
+    string x = codones.find(y);
+    cout <<x;
 }
+
+
