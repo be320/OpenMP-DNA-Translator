4172af2a104d29ae5d99d669806bdb3b0425ba92
omarsamak <omarhesham83@gmail.com>
2020-05-09 16:52:04 +0200

    print count

--- DNA-Transaltor/DNA-Transaltor/DNA-Transaltor.cpp
+++ DNA-Transaltor/DNA-Transaltor/DNA-Transaltor.cpp
@@ -4,29 +4,50 @@
 #include <sstream>
 #include <fstream>
 #include <map>
+#include <string>
 using namespace std;
+//HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+//SetConsoleTextAttribute(hConsole, k);
 
-
-void read_map(map<string, string> &m, ifstream &is){
-    string key, value;
+void read_map(map<string, int> &m, ifstream &is){
+    string key;
+    int value;
     while ( is >> key >> value ) {
     m[key] = value;
     }
 }
 
-void print_map(map<string, string> m){
-    map<string, string>::iterator itr;
+void print_map(map<string, int> m){
+    map<string, int>::iterator itr;
     for (itr = m.begin(); itr != m.end(); ++itr) {
         cout << itr->first << '\t' << itr->second << '\n';
     }
 }
 
 int main(){
-    map<string, string> codons;
+    map<string, int> codons;
 
     ifstream myfile;
     myfile.open("codon.txt");
     read_map(codons, myfile);
+    myfile.close();
+
+    myfile.open("InputSeq.dat.txt");
+    string str;
+    if (myfile) {
+        ostringstream ss;
+        ss << myfile.rdbuf(); // reading data
+        str = ss.str();
+    }
+    str.erase(remove_if(str.begin(), str.end(), [](char c) { return !isalpha(c); }), str.end());
+
+   //cout << str;
+   // print_map(codons);
+    string temp;
+    for (int i = 0;i < str.length();i+=3) {
+        temp = str.substr(i, 3);
+        codons[temp]++;
+    }
     print_map(codons);
 }
 
